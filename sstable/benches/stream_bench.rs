701d1360d9bcd5fd04a0bca6448dbdafe5da1827
trinity-1686a <trinity.pointard@datadoghq.com>
2026-09-23 12:11:10 +0200

    add bench for automaton sstable streaming

--- sstable/benches/stream_bench.rs
+++ sstable/benches/stream_bench.rs
@@ -5,9 +5,56 @@ use common::file_slice::FileSlice;
 use criterion::{Criterion, criterion_group, criterion_main};
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
+use tantivy_fst::Automaton;
 use tantivy_sstable::{Dictionary, MonotonicU64SSTable};
 
 const CHARSET: &[u8] = b"abcdefghij";
+
+// This intentionally only supports needles without repeated bytes so we don't need KMP style
+// handling.
+struct SimpleSubsliceAutomaton<'a> {
+    needle: &'a [u8],
+}
+
+impl<'a> SimpleSubsliceAutomaton<'a> {
+    fn new(needle: &'a [u8]) -> Self {
+        debug_assert!(!needle.is_empty());
+        debug_assert!(
+            needle
+                .iter()
+                .enumerate()
+                .all(|(i, byte)| !needle[..i].contains(byte))
+        );
+        Self { needle }
+    }
+}
+
+impl Automaton for SimpleSubsliceAutomaton<'_> {
+    type State = usize;
+
+    fn start(&self) -> Self::State {
+        0
+    }
+
+    fn is_match(&self, state: &Self::State) -> bool {
+        *state == self.needle.len()
+    }
+
+    fn will_always_match(&self, state: &Self::State) -> bool {
+        self.is_match(state)
+    }
+
+    fn accept(&self, state: &Self::State, byte: u8) -> Self::State {
+        if self.is_match(state) {
+            return *state;
+        }
+        if byte == self.needle[*state] {
+            *state + 1
+        } else {
+            (byte == self.needle[0]).into()
+        }
+    }
+}
 
 fn generate_key(rng: &mut impl Rng) -> String {
     let len = rng.random_range(3..12);
@@ -63,7 +110,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     });
     c.bench_function("short_scan_init_and_scan", |b| {
         b.iter(|| {
-            assert_eq!(stream_bench(&dict, b"fa", b"faz", true), 971);
+            assert_eq!(stream_bench(&dict, b"fa", b"faz", true), 1051);
         })
     });
     c.bench_function("full_scan_init_and_scan_full_with_bound", |b| {
@@ -74,6 +121,19 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("full_scan_init_and_scan_full_no_bounds", |b| {
         b.iter(|| {
             let mut stream = dict.stream().unwrap();
+            let mut count = 0;
+            while stream.advance() {
+                count += 1;
+            }
+            count
+        })
+    });
+    c.bench_function("full_scan_simple_subslice_automaton", |b| {
+        b.iter(|| {
+            let mut stream = dict
+                .search(SimpleSubsliceAutomaton::new(b"abcef"))
+                .into_stream()
+                .unwrap();
             let mut count = 0;
             while stream.advance() {
                 count += 1;
